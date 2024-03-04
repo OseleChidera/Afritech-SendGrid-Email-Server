14d0280e3b953ba0d35dfa57f5f5918bca931161
Osele Chidera <oselechideraemmanuel123@gmail.com>
2024-03-04 16:14:47 +0000

    added extra post requests to send emails on checkout and on signup

--- server.js
+++ server.js
@@ -28,6 +28,100 @@ app.get('/', (req, res) => {
 
 
 // Endpoint to send email
+app.post('/send-order-confirmation-email', async (req, res) => {
+    // console.log("logged in post request", req.body) 
+    const { fullName, userEmail, orderID, orderArray,amountLeftToPay } = req.body;
+    // console.log(fullName, userEmail, orderID, orderObject, amountPaid)
+    // console.log("req.body: ", req.body)
+
+    
+    function splitName(fullName) {
+        // Split the input string by space
+        const parts = fullName.split(' ');
+        
+        // Return the second part (index 1)
+        return parts.length > 1 ? parts[1] : " ";
+    }
+     function formatNumberWithCommas(value) {
+        // Check if value is defined and not null
+        if (value !== undefined && value !== null) {
+            // Convert the number to a string
+            let numberString = value.toString();
+
+            // Use a regular expression to add commas
+            numberString = numberString.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+            return numberString;
+        }
+
+        // Return a default value or handle the case when value is undefined or null
+        return "N/A";
+    }
+
+    console.log("an attempt was made to post a request ", req.body)
+    try {
+  
+    
+    const emailTemplate = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>You just made a payment to Afritech for order <b>#${orderID}</b></title>
+    </head>
+    <body id="email-template">
+        <p class="email-p">
+            Hello <b>${splitName(fullName)}</b>, <br>
+            You just checked out <b>#${orderID}</b> totaling <b>₦${formatNumberWithCommas(amountLeftToPay)}</b> on Afritech 
+        </p>
+    
+        <h4>Here is your order breakdown:</h4>
+        <div className="email-product-list">
+            ${orderArray.map((item) => {
+                return `
+                    <div class="email-product">
+                        <div class="email-product-image">
+                            <img src="${item.imageGalleryImages[0].imageURL}" class="" width="70" height="70" alt="product image"/>
+                        </div>
+                        <div class="email-product-title">
+                            <h3 class="">${item.name} - ₦${formatNumberWithCommas(item.price)}</h3>
+                        </div>
+                    </div>
+                `;
+            }).join('')}
+        </div>
+        
+        <p>Regards,</p>
+        <p>Afritech Team</p>
+    </body>
+    </html>
+    `;
+     
+        const msg = {
+            to: userEmail,
+            from: 'afritech19@gmail.com',
+            subject: 'You just checked out an order on Afritech',
+            text: 'and easy to do anywhere, even with Node.js',
+            html:  emailTemplate,
+        };
+     
+        // Send email using SendGrid
+        sgMail.send(msg)
+            .then(() => {
+                console.log('Email sent')
+                console.log(msg)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+        res.status(200).send('Email sent successfully');
+    } catch (error) {
+        console.error('Error sending email:', error);
+        console.error('Error sending email:', error.message, error);
+        res.status(500).send('Error sending email');
+    }
+});
+
 app.post('/send-email', async (req, res) => {
     // console.log("logged in post request", req.body) 
     const { fullName, userEmail, orderID, orderObject, amountPaid, transactionNumber, reference, amountLeftToPay } = req.body;
@@ -160,6 +254,48 @@ console.log("amountLeftToPay - amountPaid: " + eval(orderObject?.leftToPay - amo
     }
 });
 
+app.post('/send-verify-user-account', async (req, res) => {
+
+    try {
+  
+    
+    const emailTemplate = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>A new User Just Registered on Afritech</title>
+    </head>
+    <body id="email-template">
+        <p>Hooray!!! we just got a new user. Hurry up and verify their credentials</p>
+    </body>
+    </html>
+    `;
+     
+        const msg = {
+            to: "afritech19@gmail.com",
+            from: 'afritech19@gmail.com',
+            subject: 'A new User Just Registered on Afritech',
+            text: 'and easy to do anywhere, even with Node.js',
+            html:  emailTemplate,
+        };
+     
+        // Send email using SendGrid
+        sgMail.send(msg)
+            .then(() => {
+                console.log('Email sent')
+                console.log(msg)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+        res.status(200).send('Email sent successfully');
+    } catch (error) {
+        console.error('Error sending email:', error);
+        console.error('Error sending email:', error.message, error);
+        res.status(500).send('Error sending email');
+    }
+});
 // Start the server
 app.listen(port, () => {
     console.log(`You are running on port ${port}`);
